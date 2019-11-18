7cfef401787da7b37e7401c3fee741a6c589821c
spdavern <sean@lizandsean.com>
2019-11-18 14:00:26 -0800

    Initializing project repo.

---
+++ main.py
@@ -0,0 +1,13 @@
+import src.obtain
+import src.scrub
+import src.explore
+import src.model
+import src.interpret
+
+# python main.py
+if __name__ == "__main__":
+    src.obtain.run()
+    src.scrub.run()
+    src.explore.run()
+    src.model.run()
+    src.interpret.run()
---
+++ src/utils/make_dataset.py
@@ -0,0 +1,7 @@
+def run():
+    print("src.data.make_dataset.run()")
+    print("...processing raw data")
+    print("...saving interim data")
+    print("...processing interim data")
+    print("...processing external data")
+    print("...")
